d25123c51d5a29d5bbe8998a023fb748d40a5a69
Vincent Zwanenburg <vincent@Vincents-MacBook-Pro-2.local>
2023-04-11 00:08:42 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ docker run -d \
  --network=host \
  -e "RAILS_SERVE_STATIC_FILES=true" \
  -e TZ=Europe/Dublin \
- -e "MYSQL_USER=vincent" \
- -e "MYSQL_PASSWORD=Swansk1k1ng" \
+ -e "MYSQL_USER=YOUR_USER" \
+ -e "MYSQL_PASSWORD=YOUR_PASSWORD" \
  -e "MYSQL_DATABASE=plant_friend" \
  -e "MYSQL_HOST=127.0.0.1" \
  -e "MYSQL_PORT=3306" \
